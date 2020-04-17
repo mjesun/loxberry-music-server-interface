9b84a50737cb6fcfeda8c29b27e2c5d480dd5aab
Miguel Jiménez Esún <mjesun@hotmail.com>
2020-04-17 02:41:39 +0200

    Automate release process

---
+++ release.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const childProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const opts = {cwd: __dirname};
+const pluginRegExp = /(?<=^VERSION=).*$/m;
+const packageRegExp = /(?<=^\s*"version"\s*:\s*").*(?=",$)/m;
+const version = process.argv[2];
+
+function replaceInFile(file, regexp, replacement) {
+  const fullPath = path.join(__dirname, file);
+  const contents = fs.readFileSync(fullPath, 'utf-8');
+
+  fs.writeFileSync(fullPath, contents.replace(regexp, replacement));
+}
+
+// Clean al the stuff.
+childProcess.spawnSync('git', ['clean', '-xdf'], opts);
+
+// Modify files with the version.
+replaceInFile('plugin.cfg', pluginRegExp, version);
+replaceInFile('package.json', packageRegExp, version);
+replaceInFile('bin/service/package.json', packageRegExp, version);
+
+// Commit the modifications.
+childProcess.spawnSync('git', ['commit', '-m', 'Version ' + version], opts);
+
+// Generate the zip.
+childProcess.spawnSync(
+  'zip',
+
+  [
+    '-r',
+    '-9',
+    version + '.zip',
+    'bin',
+    'config',
+    'daemon',
+    'icons',
+    'uninstall',
+    'webfrontend',
+    'plugin.cfg',
+    'preupgrade.sh',
+    'postinstall.sh',
+  ],
+
+  opts,
+);
